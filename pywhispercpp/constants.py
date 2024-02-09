7064550f7ce90efe61f7942c8a055d1f3e052619
Josep Oriol Ayats <oriol.ayats@gmail.com>
2024-02-09 11:20:30 +0100

    added all the available models

--- pywhispercpp/constants.py
+++ pywhispercpp/constants.py
@@ -25,19 +25,30 @@ LOGGIN_LEVEL = logging.INFO
 
 MODELS_DIR = Path(user_data_dir(PACKAGE_NAME)) / 'models'
 
-AVAILABLE_MODELS = ["tiny.en",
-                    "tiny",
-                    "base.en",
-                    "base",
-                    "small.en",
-                    "small",
-                    "medium.en",
-                    "medium",
-                    "large-v1",
-                    "large",
-                    "large-v3",
-                    ]
-
+AVAILABLE_MODELS = [
+                "base",
+                "base-q5_1",
+                "base.en",
+                "base.en-q5_1",
+                "large-v1",
+                "large-v2",
+                "large-v2-q5_0",
+                "large-v3",
+                "large-v3-q5_0",
+                "medium",
+                "medium-q5_0",
+                "medium.en",
+                "medium.en-q5_0",
+                "small",
+                "small-q5_1",
+                "small.en",
+                "small.en-q5_1",
+                "tiny",
+                "tiny-q5_1",
+                "tiny.en",
+                "tiny.en-q5_1",
+                "tiny.en-q8_0",
+                ]
 PARAMS_SCHEMA = {  # as exactly presented in whisper.cpp
     'n_threads': {
             'type': int,
