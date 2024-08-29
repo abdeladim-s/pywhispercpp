7bf3243abe7c0087963c0f53fa5a38bc748075f7
su <sadiki.abdeladim@gmail.com>
2024-08-28 23:25:05 -0400

    fix linux: #46

--- pywhispercpp/__init__.py
+++ pywhispercpp/__init__.py
@@ -5,7 +5,8 @@ from glob import glob
 lib_dir = os.path.join(os.path.dirname(__file__), 'lib/*')
 
 # Append lib dir to PATH
-os.add_dll_directory(os.path.join(os.path.dirname(__file__), 'lib'))
+if os.name == 'nt':
+    os.add_dll_directory(os.path.join(os.path.dirname(__file__), 'lib'))
 
 for file in glob(lib_dir):
     ctypes.CDLL(os.path.join(os.path.dirname(__file__), 'lib', file))
