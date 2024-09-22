cd914ee24a7b40603832e46ac33813014ecfdf50
Issac Dowling <git@issacdowling.com>
2024-09-22 13:12:14 +0100

    Match type hints by providing new_segment_callback with a Segment, not a list[Segment]

--- pywhispercpp/model.py
+++ pywhispercpp/model.py
@@ -261,7 +261,7 @@ class Model:
         n = pw.whisper_full_n_segments(ctx)
         start = n - n_new
         res = Model._get_segments(ctx, start, n)
-        Model._new_segment_callback(res)
+        Model._new_segment_callback(res[0])
 
     @staticmethod
     def _load_audio(media_file_path: str) -> np.array:
