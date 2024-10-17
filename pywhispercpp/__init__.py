fe405a6347d2dce3c728d0d73a76f905fca93ee6
Piotr Czapla <me@piotrczapla.com>
2024-10-17 13:54:52 +0200

    Fix the missing CoreML libraries issue.
    This is similar to the GGML problem where the Whisper library has incorrect linking info, causing it to struggle to find the libraries. Manually loading them works as a temporary fix, but we should really clean up the dylib links using a tool like RepairWheel for a proper solution. See the next commit.

--- pywhispercpp/__init__.py
+++ pywhispercpp/__init__.py
@@ -4,11 +4,12 @@ from glob import glob
 
 # ggml should be loaded first
 ggml_libs = glob(os.path.join(os.path.dirname(__file__), 'lib/*ggml*'))
+coreml_libs = glob(os.path.join(os.path.dirname(__file__), 'lib/*coreml*'))
 libs = glob(os.path.join(os.path.dirname(__file__), 'lib/*'))
 
 # Append lib dir to PATH
 if os.name == 'nt':
     os.add_dll_directory(os.path.join(os.path.dirname(__file__), 'lib'))
 
-for file in ggml_libs + libs:
+for file in ggml_libs + coreml_libs + libs:
     ctypes.CDLL(os.path.join(os.path.dirname(__file__), 'lib', file))
