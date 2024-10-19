41545c8638f3a444bab26509738a70e3b02d18b2
abdeladim-s <sadiki.abdeladim@gmail.com>
2024-10-19 15:24:57 -0400

    delete manual lib loading

--- pywhispercpp/__init__.py
+++ pywhispercpp/__init__.py
@@ -1,15 +1,1 @@
-import os
-import ctypes
-from glob import glob
 
-# ggml should be loaded first
-ggml_libs = glob(os.path.join(os.path.dirname(__file__), 'lib/*ggml*'))
-coreml_libs = glob(os.path.join(os.path.dirname(__file__), 'lib/*coreml*'))
-libs = glob(os.path.join(os.path.dirname(__file__), 'lib/*'))
-
-# Append lib dir to PATH
-if os.name == 'nt':
-    os.add_dll_directory(os.path.join(os.path.dirname(__file__), 'lib'))
-
-for file in ggml_libs + coreml_libs + libs:
-    ctypes.CDLL(os.path.join(os.path.dirname(__file__), 'lib', file))
